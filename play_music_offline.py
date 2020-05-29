6461e72dd69b3231c208e422ece88f8c24b459bb
Sarath <49370420+sarath1398@users.noreply.github.com>
2020-05-29 21:32:50 +0530

    Generalized play music offline

--- play_music_offline.py
+++ play_music_offline.py
@@ -4,8 +4,8 @@ import random
 #from mutagen.mp3 import MP3 
 from time import sleep
 
-os.chdir(r'F:\Themes')
-playlist=[i for i in os.listdir(os.getcwd()) if i[-4:]=='.mp3']
+os.chdir(r'{}'.format(input()))  #Get the path for your songs folder
+playlist=[i for i in os.listdir(os.getcwd()) if i[-4:]=='.mp3'] 
 random.shuffle(playlist)
 
 for song in playlist:
